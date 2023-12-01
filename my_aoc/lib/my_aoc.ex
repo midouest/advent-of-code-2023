4fd0586bc0949c58d000c80b830cb1db4a1659fd
Robin <midouesterner@gmail.com>
2023-11-30 22:35:53 -0600

    new instead of client

--- my_aoc/lib/my_aoc.ex
+++ my_aoc/lib/my_aoc.ex
@@ -10,7 +10,7 @@ defmodule MyAOC do
   defstruct [:year, :day]
   alias __MODULE__, as: Client
 
-  def client(year, day) do
+  def new(year, day) do
     %Client{
       year: year,
       day: day
